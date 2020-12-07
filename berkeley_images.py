ab316657f2e5bbf7fe5a7aa5c13d5639b25afbc5
Mathias Winkel <mathias.winkel@merckgroup.com>
2020-12-07 12:28:32 +0100

    prepare using the same basis for the different color channels: UI adaptations

--- berkeley_images.py
+++ berkeley_images.py
@@ -14,13 +14,24 @@ import numpy as np
 import matplotlib.pyplot as plt
 from TransformInvariantNMF import SparseNMF, ShiftInvariantNMF
 
+COLOR_SELECTIONS = {
+    'grey': lambda img: [np.dot(img[..., :3], [0.2989, 0.5870, 0.1140])[:, :, np.newaxis]],
+    'red': lambda img: [img[..., 0:1]],
+    'green': lambda img: [img[..., 1:2]],
+    'blue': lambda img: [img[..., 2:3]],
+    'color basis': lambda img: [img],
+    'colors (identical basis)': lambda img: [img[..., 0:1], img[..., 1:2], img[..., 2:3]],
+}
+
+COLOR_SELECTIONS_KEYS = list(COLOR_SELECTIONS.keys())
+
 
 def st_define_dataset_params() -> (str, str, int, int):
     st.sidebar.markdown('# Image Dataset')
     d = st.sidebar.text_input('File path', value=r'BSR_bsds500/BSR/BSDS500/data/images/train')
     f = st.sidebar.text_input('File filter', value='*.jpg')
     m = st.sidebar.number_input('Max images', min_value=0, value=5)
-    c = st.sidebar.radio('Channel(s)', ['grey', 'red', 'green', 'blue', 'color'], 0)
+    c = st.sidebar.radio('Channel(s)', COLOR_SELECTIONS_KEYS, 0)
 
     return d, f, m, c
 
@@ -67,46 +78,42 @@ def st_define_nmf_params(image_shape: tuple) -> dict:
 
 
 @st.cache
-def load_images(path: str, pattern: str, max_images: int = 0, color_mode: int = 'grey') -> (np.ndarray, tuple):
+def load_images(path: str, pattern: str, max_images: int = 0,
+                color_mode: int = 'grey', dtype=np.float32) -> (np.ndarray, tuple):
     images = []
     rows, cols = 10000, 10000
 
-    for filename in glob.glob(os.path.join(os.getcwd(), path, pattern)):
-        logging.info(filename)
-
-        img = imageio.imread(filename)[:, :, :3] / 255.
-
-        if color_mode == 'grey':
-            img = np.dot(img[..., :3], [0.2989, 0.5870, 0.1140])[:, :, np.newaxis]
-        elif color_mode == 'red':
-            img = img[..., 0:1]
-        elif color_mode == 'green':
-            img = img[..., 1:2]
-        elif color_mode == 'blue':
-            img = img[..., 2:3]
-        elif color_mode == 'color':
-            pass
-        else:
-            raise ValueError(f'Unsupported color mode: {color_mode}.')
-
-        # flip all images to landscape orientation
-        if img.shape[1] < img.shape[0]:
-            img = np.swapaxes(img, 0, 1)
-
-        images.append(img)
-
-        rows, cols = min(rows, img.shape[0]), min(cols, img.shape[1])
-
-        if 0 < max_images <= len(images):
+    count = 0
+    for count, filename in enumerate(glob.glob(os.path.join(os.getcwd(), path, pattern))):
+        # limit number of images
+        if 0 < max_images <= count:
             break
 
+        logging.info(filename)
+
+        img = (imageio.imread(filename)[:, :, :3] / 255.).astype(dtype)
+        channels = COLOR_SELECTIONS[color_mode](img)
+
+        # flip all images to landscape orientation
+        channels = [np.swapaxes(ch, 0, 1) if ch.shape[1] < ch.shape[0] else ch for ch in channels]
+
+        for channel in channels:
+            rows, cols = min(rows, channel.shape[0]), min(cols, channel.shape[1])
+            images.append(channel)
+
     # cut all images to fit the smallest image
-    map(lambda img: img[:rows, :cols, :], images)
+    images = [image[:rows, :cols, :] for image in images]
 
-    images = np.array(images)
+    images = np.asarray(images)
 
-    # roll image index to the last dimension
+    # roll image index to the last dimension because this is how the NMF needs its data
     images = np.moveaxis(images, 0, -1)
+
+    # just ensure nothing was garbled in the code above
+    assert (images.ndim == 4)
+    assert (images.shape[0] == rows and images.shape[1] == cols)
+    assert (images.shape[2] in (1, 3))
+    assert (images.shape[3] in (count, 3 * count))
 
     return images, (rows, cols)
 
@@ -140,39 +147,31 @@ def st_compare_single_signals(nmf, signal_number):
         st.pyplot(fig)
 
 
-def st_show_dictionary(W, color_mode):
-    def plot_dictionary(W, channel):
+def st_show_dictionary(W, num_columns=10):
+    def plot_dictionary(W):
         figs = []
         for m in range(W.shape[-1]):
             fig = plt.figure()
-            plt.imshow(W[..., channel, m])
+            plt.imshow(W[..., m])
             plt.grid(False)
             plt.axis('off')
             plt.tight_layout()
             figs.append(fig)
         return figs
 
-    st.markdown('# Learned dictionary')
-    for channel in range(W.shape[-2]):
-        if color_mode == 'color':
-            color = ['red', 'green', 'blue'][channel]
-        else:
-            color = color_mode
-
-        st.markdown(f'## {color}')
-        figs = plot_dictionary(W, channel)
-        for row in chunked(figs, 10):
-            cols = st.beta_columns(10)
-            for fig, col in zip(row, cols):
-                with col:
-                    st.pyplot(fig)
+    st.markdown(f'# Learned dictionary: {color_mode}')
+    figs = plot_dictionary(W)
+    for row in chunked(figs, num_columns):
+        cols = st.beta_columns(num_columns)
+        for fig, col in zip(row, cols):
+            with col:
+                st.pyplot(fig)
 
 
 def st_plot_activations(H, nmf_params, signal_number):
-
     st.markdown('# Activation pattern')
 
-    m = st.slider('Dictionary atom', min_value=0, max_value=H.shape[-2]-1)
+    m = st.slider('Dictionary atom', min_value=0, max_value=H.shape[-2] - 1)
     fig = plt.figure(figsize=(10, 5))
     if nmf_params['shift_invariant']:
         plt.imshow(H[..., m, signal_number], aspect='auto')
@@ -211,7 +210,7 @@ if __name__ == '__main__':
     st_compare_single_signals(nmf, signal_number)
 
     # show learned dictionary
-    st_show_dictionary(nmf.W, color_mode)
+    st_show_dictionary(nmf.W)
 
     # show activation pattern
     st_plot_activations(nmf.H, nmf_params, signal_number)
