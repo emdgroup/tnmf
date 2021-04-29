3989657aea8a732f97fabb4a8fe8ab3b14217cab
Mathias Winkel <mathias.winkel@merckgroup.com>
2021-04-29 10:38:56 +0200

    prepare skeleton for sphinx documentation

---
+++ doc/conf.py
@@ -0,0 +1,70 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Path setup --------------------------------------------------------------
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
+import os
+import sys
+sys.path.insert(0, os.path.abspath('..'))
+
+
+# -- Project information -----------------------------------------------------
+
+project = 'TransformInvariantNMF'
+copyright = '2021, Adrian Sosic, Mathias Winkel'
+author = 'Adrian Sosic, Mathias Winkel'
+
+
+# -- General configuration ---------------------------------------------------
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+extensions = [
+    'autoapi.extension',
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.napoleon',    # https://www.sphinx-doc.org/en/master/usage/extensions/napoleon.html#module-sphinx.ext.napoleon
+]
+
+autoapi_type = 'python'
+autoapi_dirs = ['../tnmf', ]
+autoapi_options = [ 'members', 'undoc-members', 'no-private-members', 'show-inheritance', 'show-module-summary', 'special-members', 'imported-members', ]
+
+
+
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ['_templates']
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+# -- Options for HTML output -------------------------------------------------
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+html_theme = 'sphinxdoc'
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ['_static']
+
+
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/{.major}'.format(sys.version_info), None),
+    'numpy': ('https://numpy.org/doc/stable', None),
+    'sklearn': ('https://scikit-learn.org/stable', None),
+    'scipy': ('https://docs.scipy.org/doc/scipy/reference', None),
+    "torch": ("https://pytorch.org/docs/master/", None),
+    }
---
+++ doc/index.rst
@@ -0,0 +1,20 @@
+.. TransformInvariantNMF documentation master file, created by
+   sphinx-quickstart on Fri Apr 23 15:08:06 2021.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to TransformInvariantNMF's documentation!
+=================================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+   autoapi/index
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
