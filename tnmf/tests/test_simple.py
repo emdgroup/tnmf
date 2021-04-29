577e60283088346d1b5e0c80f7e97032f8ab852d
Mathias Winkel <mathias.winkel@merckgroup.com>
2021-04-29 14:44:52 +0200

    add a very simple test to ensure the code runs at all (can and should be extended to the other backends later)

---
+++ tnmf/tests/test_simple.py
@@ -0,0 +1,48 @@
+"""
+Test if a single sample decomposition works
+"""
+
+from scipy.misc import face
+import numpy as np
+from tnmf.TransformInvariantNMF import TransformInvariantNMF
+import logging
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s: %(message)s')
+
+
+def do_test(backend: str, expected_error: float):
+    np.random.seed(seed=42)
+
+    img = face(gray=True) / 255
+
+    # downsample for higher speed
+    img = img[::4, ::4] + img[1::4, ::4] + img[::4, 1::4] + img[1::4, 1::4]
+    img /= 4.0
+
+    img = img[np.newaxis, np.newaxis, ...]
+
+    patch_shape = (7, 7)
+
+    nmf = TransformInvariantNMF(
+        n_atoms=10,
+        atom_shape=patch_shape,
+        n_iterations=25,
+        backend=backend,
+        logger=None,
+        verbose=3,
+    )
+
+    nmf.fit(img)
+
+    assert np.isclose(nmf._energy_function(img), expected_error)
+
+    img_r = nmf.reconstruct()
+    assert np.isclose(0.5*np.sum(np.square(img_r - img)), expected_error)
+
+
+def test_numpy_fft():
+    do_test('numpy_fft', 104.74284)
+
+
+#def test_numpy_caching_fft():
+#    do_test('numpy_caching_fft', 104.0667)
