e48dc5d254675a4e034a7d864d411983b6ee2d2a
Adrian Sosic <adrian.sosic@merckgroup.com>
2021-04-30 14:14:58 +0200

    add intermediate PyTorchBackend class

--- tnmf/backends/Backend.py
+++ tnmf/backends/Backend.py
@@ -1,10 +1,29 @@
 # TODO: generalize n_transforms from numpy_fft to all backends
 # TODO: all backends need to support self._mode_R et al
 # TODO: do we need self._input_padding ? If yes, all backends have to support it.
+# TODO: fix numpy to torch dtype
+# TODO: refactor common backend logic of NumpyBackend/PyTorchBackend into function
 
 import abc
+import torch
 import numpy as np
 from typing import Tuple, Optional
+from torch import Tensor
+
+# see https://github.com/pytorch/pytorch/issues/40568#issuecomment-649961327
+numpy_to_torch_dtype_dict = {
+    np.uint8: torch.uint8,
+    np.int8: torch.int8,
+    np.int16: torch.int16,
+    np.int32: torch.int32,
+    np.int64: torch.int64,
+    np.float16: torch.float16,
+    np.float32: torch.float32,
+    np.float64: torch.float64,
+    np.dtype('float64'): torch.float64,
+    np.complex64: torch.complex64,
+    np.complex128: torch.complex128
+}
 
 
 class Backend(metaclass=abc.ABCMeta):
@@ -72,5 +91,24 @@ class NumpyBackend(Backend):
 
         if W is None:
             W = np.asarray(1 - np.random.rand(n_atoms, self.n_channels, *atom_shape), dtype=V.dtype)
+
+        return W, H
+
+
+class PyTorchBackend(Backend):
+
+    def initialize_matrices(
+            self,
+            V: np.ndarray,
+            atom_shape: Tuple[int, ...],
+            n_atoms: int,
+            W: Optional[Tensor] = None,
+    ) -> Tuple[Tensor, Tensor]:
+
+        self.dtype = numpy_to_torch_dtype_dict[V.dtype]
+        H = (1 - torch.rand((self.n_samples, n_atoms, *self._transform_shape), dtype=self.dtype))
+
+        if W is None:
+            W = (1 - torch.rand((n_atoms, self.n_channels, *atom_shape), dtype=self.dtype))
 
         return W, H
--- tnmf/backends/PyTorch.py
+++ tnmf/backends/PyTorch.py
@@ -1,29 +1,15 @@
-# TODO: fix numpy to torch dtype
 # TODO: it should be possible to reformulate the gradients using
 #       https://pytorch.org/docs/stable/autograd.html#torch.autograd.functional.jacobian
 # TODO: merge gradient functions into one
 # TODO: add device option
 
-from .Backend import Backend
+from .Backend import PyTorchBackend
 from torch import Tensor
 import torch
 import numpy as np
 from typing import Tuple, Optional
 
-# see https://github.com/pytorch/pytorch/issues/40568#issuecomment-649961327
-numpy_to_torch_dtype_dict = {
-    np.uint8: torch.uint8,
-    np.int8: torch.int8,
-    np.int16: torch.int16,
-    np.int32: torch.int32,
-    np.int64: torch.int64,
-    np.float16: torch.float16,
-    np.float32: torch.float32,
-    np.float64: torch.float64,
-    np.dtype('float64'): torch.float64,
-    np.complex64: torch.complex64,
-    np.complex128: torch.complex128
-}
+
 
 conv_dict = {
     1: torch.nn.functional.conv1d,
@@ -32,23 +18,7 @@ conv_dict = {
 }
 
 
-class PyTorch_Backend(Backend):
-
-    def initialize_matrices(
-            self,
-            V: np.ndarray,
-            atom_shape: Tuple[int, ...],
-            n_atoms: int,
-            W: Optional[Tensor] = None,
-    ) -> Tuple[Tensor, Tensor]:
-
-        self.dtype = numpy_to_torch_dtype_dict[V.dtype]
-        H = (1 - torch.rand((self.n_samples, n_atoms, *self._transform_shape), dtype=self.dtype))
-
-        if W is None:
-            W = (1 - torch.rand((n_atoms, self.n_channels, *atom_shape), dtype=self.dtype))
-
-        return W, H
+class PyTorch_Backend(PyTorchBackend):
 
     def normalize(self, arr: Tensor, axes: Tuple[int]) -> Tensor:
         return arr / (arr.sum(dim=axes, keepdim=True))
