8b7ce2f1d0120891b0f291d5339eb41865433667
Adrian Sosic <adrian.sosic@merckgroup.com>
2021-05-07 08:16:30 +0200

    initialize matrices in Backend class and cast result in PyTorchBackend

--- tnmf/backends/_PyTorchBackend.py
+++ tnmf/backends/_PyTorchBackend.py
@@ -1,27 +1,12 @@
 # pylint: disable=abstract-method
 
-from typing import Tuple, Optional
+from typing import Tuple, Optional, Union
 
 import numpy as np
 import torch
 from torch import Tensor
 
 from ._Backend import Backend
-
-# see https://github.com/pytorch/pytorch/issues/40568#issuecomment-649961327
-numpy_to_torch_dtype_dict = {
-    np.uint8: torch.uint8,
-    np.int8: torch.int8,
-    np.int16: torch.int16,
-    np.int32: torch.int32,
-    np.int64: torch.int64,
-    np.float16: torch.float16,
-    np.float32: torch.float32,
-    np.float64: torch.float64,
-    np.dtype('float64'): torch.float64,
-    np.complex64: torch.complex64,
-    np.complex128: torch.complex128
-}
 
 
 class PyTorchBackend(Backend):
@@ -31,13 +16,14 @@ class PyTorchBackend(Backend):
         V: np.ndarray,
         atom_shape: Tuple[int, ...],
         n_atoms: int,
-        W: Optional[Tensor] = None,
+        W: Optional[Union[np.ndarray, Tensor]] = None,
     ) -> Tuple[Tensor, Tensor]:
 
-        H = torch.from_numpy(np.asarray(1 - np.random.rand(self.n_samples, n_atoms, *self._transform_shape), dtype=V.dtype))
+        W, H = super()._initialize_matrices(V, atom_shape, n_atoms, W)
 
-        if W is None:
-            W = torch.from_numpy(np.asarray(1 - np.random.rand(n_atoms, self.n_channels, *atom_shape), dtype=V.dtype))
+        H = torch.from_numpy(H)
+        if W is not None:
+            W = torch.as_tensor(W)
 
         return W, H
 
