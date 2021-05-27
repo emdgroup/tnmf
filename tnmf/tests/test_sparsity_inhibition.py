b99db958c2c9e2a588eedee6fff4376ff42373a2
Mathias Winkel <mathias.winkel@merckgroup.com>
2021-05-27 10:24:10 +0200

    update reference values, add another test with reduced inhibition range

--- tnmf/tests/test_sparsity_inhibition.py
+++ tnmf/tests/test_sparsity_inhibition.py
@@ -19,20 +19,26 @@ backends = ['numpy', 'numpy_fft', 'numpy_caching_fft', 'pytorch']
 # hard-coded expected energy levels for the different reconstruction modes
 test_params_sparsity = [
     # increasing sparsity, no inhibition
-    (dict(sparsity_H=0.0, inhibition_strength=0.0), dict(energy=186.666013, norm_H_1=7704.38977, norm_H_0=176346)),
-    (dict(sparsity_H=0.1, inhibition_strength=0.0), dict(energy=225.494731, norm_H_1=6563.91176, norm_H_0=174037)),
-    (dict(sparsity_H=0.5, inhibition_strength=0.0), dict(energy=858.621063, norm_H_1=4258.82541, norm_H_0=155247)),
-    (dict(sparsity_H=1.0, inhibition_strength=0.0), dict(energy=2429.69334, norm_H_1=2114.50047, norm_H_0=136396)),
-    (dict(sparsity_H=5.0, inhibition_strength=0.0), dict(energy=5351.91865, norm_H_1=3.0800e-06, norm_H_0=65338)),
-    (dict(sparsity_H=10., inhibition_strength=0.0), dict(energy=5351.91866, norm_H_1=2.5103e-13, norm_H_0=62486)),
+    {'fit': dict(sparsity_H=0.0), 'nmf': dict(), 'res': dict(energy=186.666013, norm_H_1=7704.38977, norm_H_0=176346)},
+    {'fit': dict(sparsity_H=0.1), 'nmf': dict(), 'res': dict(energy=225.494731, norm_H_1=6563.91176, norm_H_0=174037)},
+    {'fit': dict(sparsity_H=0.5), 'nmf': dict(), 'res': dict(energy=858.621063, norm_H_1=4258.82541, norm_H_0=155247)},
+    {'fit': dict(sparsity_H=1.0), 'nmf': dict(), 'res': dict(energy=2429.69334, norm_H_1=2114.50047, norm_H_0=136396)},
+    {'fit': dict(sparsity_H=5.0), 'nmf': dict(), 'res': dict(energy=5351.91865, norm_H_1=3.0800e-06, norm_H_0=65338)},
+    {'fit': dict(sparsity_H=10.), 'nmf': dict(), 'res': dict(energy=5351.91866, norm_H_1=2.5103e-13, norm_H_0=62486)},
 ]
 test_params_inhibition = [
     # no sparsity, increasing inhibition
-    (dict(sparsity_H=0.0, inhibition_strength=0.1), dict(energy=234.838968, norm_H_1=6730.89543, norm_H_0=176347)),
-    (dict(sparsity_H=0.0, inhibition_strength=0.5), dict(energy=680.585424, norm_H_1=5177.87844, norm_H_0=174277)),
-    (dict(sparsity_H=0.0, inhibition_strength=1.0), dict(energy=1119.00855, norm_H_1=4657.19574, norm_H_0=168777)),
-    (dict(sparsity_H=0.0, inhibition_strength=5.0), dict(energy=518.936361, norm_H_1=6872.57858, norm_H_0=100488)),
-    (dict(sparsity_H=0.0, inhibition_strength=10.), dict(energy=489.935256, norm_H_1=7224.76002, norm_H_0=62017)),
+    {'fit': dict(inhibition_strength=0.1), 'nmf': dict(), 'res': dict(energy=435.993252, norm_H_1=5574.6769, norm_H_0=175483)},
+    {'fit': dict(inhibition_strength=0.5), 'nmf': dict(), 'res': dict(energy=1831.92669, norm_H_1=3031.4130, norm_H_0=168931)},
+    {'fit': dict(inhibition_strength=1.0), 'nmf': dict(), 'res': dict(energy=2665.70594, norm_H_1=2252.1331, norm_H_0=160644)},
+    {'fit': dict(inhibition_strength=5.0), 'nmf': dict(), 'res': dict(energy=3618.33671, norm_H_1=1947.1969, norm_H_0=129929)},
+    {'fit': dict(inhibition_strength=10.), 'nmf': dict(), 'res': dict(energy=3779.64954, norm_H_1=1926.9570, norm_H_0=118795)},
+    # no sparsity, increasing inhibition, smaller inhibition range
+    {'fit': dict(inhibition_strength=0.1), 'nmf': dict(inhibition_range=((3, 3))), 'res': dict(energy=234.838968, norm_H_1=6730.89543, norm_H_0=176347)},
+    {'fit': dict(inhibition_strength=0.5), 'nmf': dict(inhibition_range=((3, 3))), 'res': dict(energy=680.585424, norm_H_1=5177.87844, norm_H_0=174277)},
+    {'fit': dict(inhibition_strength=1.0), 'nmf': dict(inhibition_range=((3, 3))), 'res': dict(energy=1119.00855, norm_H_1=4657.19574, norm_H_0=168777)},
+    {'fit': dict(inhibition_strength=5.0), 'nmf': dict(inhibition_range=((3, 3))), 'res': dict(energy=518.936361, norm_H_1=6872.57858, norm_H_0=100488)},
+    {'fit': dict(inhibition_strength=10.), 'nmf': dict(inhibition_range=((3, 3))), 'res': dict(energy=489.935256, norm_H_1=7224.76002, norm_H_0=62017)},
 ]
 
 # create the input by concatenating the test image twice
@@ -51,8 +57,9 @@ def _do_test(backend, params):
         n_iterations=25,
         backend=backend,
         verbose=3,
+        **params['nmf']
     )
-    nmf.fit(V, **params[0])
+    nmf.fit(V, **params['fit'])
 
     H = nmf.H
 
@@ -62,7 +69,7 @@ def _do_test(backend, params):
 
     nmf._logger.debug(f'energy={energy}, norm_H_1={norm_H_1}, norm_H_0={norm_H_0}')  # pylint: disable=protected-access
 
-    expectation = params[1]
+    expectation = params['res']
     assert np.isclose(energy, expectation['energy'])
     assert np.isclose(norm_H_1, expectation['norm_H_1'])
     assert np.isclose(norm_H_0, expectation['norm_H_0'])
