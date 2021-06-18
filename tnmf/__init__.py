04a81f003aa851e3e785dceee1ee768153a7f6fe
Adrian Sosic <adrian.sosic@merckgroup.com>
2021-06-18 13:53:43 +0200

    fix importlib issue for different python versions

--- tnmf/__init__.py
+++ tnmf/__init__.py
@@ -1,5 +1,8 @@
 # pylint: skip-file
-from importlib.metadata import version, PackageNotFoundError
+try:
+    from importlib.metadata import version, PackageNotFoundError
+except ImportError:  # for Python<3.8
+    from importlib_metadata import version, PackageNotFoundError
 
 try:
     __version__ = version("tnmf")
