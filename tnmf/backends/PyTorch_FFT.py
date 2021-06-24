5ccda18e4741bf2f26c6b2085e9fea62dd03f7e2
Mathias Winkel <mathias.winkel@merckgroup.com>
2021-06-24 12:19:38 +0200

    implement Pytorch_FFT_Backend

--- tnmf/backends/PyTorch_FFT.py
+++ tnmf/backends/PyTorch_FFT.py
@@ -3,21 +3,12 @@ A module that provides a PyTorch based backend for computing the gradients of th
 Shift-invariance is implemented via efficient convolution operations in Fourier space.
 """
 
-# TODO: use torch.fft.rfftn() to generalize for more dimensions and improve performance
-
 import torch
 from torch import Tensor
 from torch.nn.functional import pad
+from torch.fft import rfftn, irfftn
 
 from ._PyTorchBackend import PyTorchBackend
-
-
-#: Lookup table for the convolution function with different dimensionality
-_CONV_DICT = {
-    1: torch.nn.functional.conv1d,
-    2: torch.nn.functional.conv2d,
-    3: torch.nn.functional.conv3d,
-}
 
 
 class PyTorch_FFT_Backend(PyTorchBackend):
@@ -26,22 +17,36 @@ class PyTorch_FFT_Backend(PyTorchBackend):
     """
 
     def reconstruct(self, W: Tensor, H: Tensor) -> Tensor:
-        # TODO: support dimensions > 3
-        # TODO: consider transposed convolution as alternative
-
-        raise NotImplementedError
+        """
+        Compute sum_m    H[n, m, _, ... ] * W[_ , m, c, ...]   --> R[n, c, ...]
+        """
 
         n_shift_dimensions = W.ndim - 2
-
-        assert n_shift_dimensions <= 3
-        conv_fun = _CONV_DICT[n_shift_dimensions]
-        flip_dims = list(range(-n_shift_dimensions, 0))
-        W_flipped = torch.flip(W, flip_dims)
 
         if self._padding is None:
             H_padded = H
         else:
             H_padded = pad(H, **self._padding)
 
-        R = conv_fun(H_padded, torch.swapaxes(W_flipped, 0, 1))
-        return R
+        shape = H_padded.shape[-n_shift_dimensions:]
+        fft_shape = shape
+
+        # if dim is not specified, the last len(s) dimensions are transformed, which is exactly what we need
+        W_fft = rfftn(W, s=fft_shape)
+        H_fft = rfftn(H_padded, s=fft_shape)
+
+        # insert singleton dimensions to ensure broadcasting works as required
+        W_fft = torch.unsqueeze(W_fft, 0)
+        H_fft = torch.unsqueeze(H_fft, 2)
+
+        R_fft = W_fft * H_fft
+
+        R_fft = R_fft.sum(dim=1, keepdim=False)
+
+        fslice = (slice(None), ) * 2
+        fslice += tuple(slice(min(H_padded.shape[a], W.shape[a]) - 1,
+                              max(H_padded.shape[a], W.shape[a])) for a in range(-n_shift_dimensions, 0))
+
+        R = irfftn(R_fft, s=shape)
+
+        return R[fslice]
