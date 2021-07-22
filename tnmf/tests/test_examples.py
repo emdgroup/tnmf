f68e47ec1761d4add7b61034ab701c00669dcd16
Mathias Winkel <mathias.winkel@merckgroup.com>
2021-07-22 10:33:16 +0200

    proper output of stdout and stderr

--- tnmf/tests/test_examples.py
+++ tnmf/tests/test_examples.py
@@ -6,8 +6,10 @@ Test if all examples can be run without error, i.e.
 import sys
 import subprocess  # noqa: S404
 import pkgutil
+import logging
 import pytest
 
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s: %(message)s')
 
 EXAMPLES = [name for _, name, _ in pkgutil.iter_modules(['examples'])]
 
@@ -21,9 +23,18 @@ from examples import {}
 
 @pytest.mark.parametrize('example', EXAMPLES)
 def test_example(example: str):
-    example_run = subprocess.run([sys.executable,  # noqa: S603
-                                  "-c", EXAMPLE_FRAME.format(example)],
-                                 capture_output=True,
-                                 check=False)
 
-    assert example_run.returncode == 0
+    logger = logging.getLogger(example)
+    logger.setLevel(logging.INFO)
+
+    with subprocess.Popen([sys.executable,  # noqa: S603
+                          "-c", EXAMPLE_FRAME.format(example)],
+                          stdout=subprocess.PIPE,
+                          stderr=subprocess.PIPE) as example_run:
+        stdout, stderr = example_run.communicate()
+        for stream in [stdout, stderr]:
+            if len(stream) > 0:
+                for line in stream.decode().split('\n'):
+                    logger.info(line)
+
+        assert example_run.returncode == 0
