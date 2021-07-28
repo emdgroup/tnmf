c735ffab06c82b8ba8e5520883a7a684da6d6c3a
Mathias Winkel <mathias.winkel@merckgroup.com>
2021-07-28 11:43:25 +0200

    include examples in code coverage reporting

--- tnmf/tests/test_examples.py
+++ tnmf/tests/test_examples.py
@@ -3,7 +3,9 @@ Test if all examples can be run without error, i.e. `python <example>.py` yields
 """
 
 import sys
+import os
 import subprocess  # noqa: S404
+from tempfile import mkstemp
 import pkgutil
 import logging
 import pytest
@@ -12,8 +14,9 @@ logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s: %(m
 
 EXAMPLES = [name for _, name, _ in pkgutil.iter_modules(['examples'])]
 
-
 EXAMPLE_FRAME = """
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 plt.ion()
 from examples import {}
@@ -26,14 +29,20 @@ def test_example(example: str):
     logger = logging.getLogger(example)
     logger.setLevel(logging.INFO)
 
-    with subprocess.Popen([sys.executable,  # noqa: S603
-                          "-c", EXAMPLE_FRAME.format(example)],
-                          stdout=subprocess.PIPE,
-                          stderr=subprocess.PIPE) as example_run:
-        stdout, stderr = example_run.communicate()
-        for stream in [stdout, stderr]:
-            if len(stream) > 0:
-                for line in stream.decode().split('\n'):
-                    logger.info(line)
+    fd, path = mkstemp(suffix='.py')
+    try:
+        with os.fdopen(fd, 'w') as tmp:
+            tmp.write(EXAMPLE_FRAME.format(example))
 
-        assert example_run.returncode == 0
+        with subprocess.Popen([sys.executable, '-m', 'coverage', 'run', path],  # noqa: S603
+                              stdout=subprocess.PIPE,
+                              stderr=subprocess.PIPE) as example_run:
+            stdout, stderr = example_run.communicate()
+            for stream in [stdout, stderr]:
+                if len(stream) > 0:
+                    for line in stream.decode().split('\n'):
+                        logger.info(line)
+
+            assert example_run.returncode == 0
+    finally:
+        os.remove(path)
